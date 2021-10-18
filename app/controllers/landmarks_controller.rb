c23f851752176b44c65d7b41a35dbd92b5732260
Clay Stewart <c.stewart89@gmail.com>
2021-10-18 14:30:19 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @titles = Title.all
+    erb :'/landmarks/edit'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+  
+  patch '/landmarks/:id' do
+    puts params
+    landmark = Landmark.find_by_id(params[:id])
+    landmark.update(params[:landmark])
+
+    redirect "/landmarks/#{landmark.id}"
+  end
 end
